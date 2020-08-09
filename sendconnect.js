f110cf7a183c617bd37ee8107ac253b13c17dcef
krishna <kkrajus777@gmail.com>
2020-08-08 21:01:48 -0500

    done connection

--- sendconnect.js
+++ sendconnect.js
@@ -32,7 +32,7 @@ try {
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless:false, slowMo: 100
+        headless:false, slowMo: 75
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
@@ -40,8 +40,8 @@ try {
     await page.goto('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin');
     const userAgent = await page.evaluate(() => navigator.userAgent );
     // await page.waitFor(2000)
-    await page.type('#username', '***')
-    await page.type('#password', "***")
+    await page.type('#username', '')
+    await page.type('#password', "")
     await page.click('[aria-label="Sign in"]')
 
     let connectionReqSent = 0
@@ -64,22 +64,31 @@ try {
             for (let i = 0; i <= buttons.length; i++) {
                 let currButton = await buttons[i]
                 let connButton = await page.evaluate(currButton => currButton && currButton.innerText, currButton)
-
-                if (connButton === 'Connect') {
+                if (connButton && connButton.trim().toLowerCase() === 'connect') {
+                    // await page.evaluate(currButton => currButton && currButton.scrollIntoView(), currButton)
+                    await page.waitFor(1000)
                     currButton.click()
                     await page.waitFor(2000)
                     await page.waitForSelector('[aria-label="Add a note"]')
-                    await page.waitFor(5000)
+                    await page.waitFor(2000)
                     await page.click('[aria-label="Add a note"]')
                     await page.waitForSelector('#custom-message')
-                    await page.type(
-                        '#custom-message',
-                        marketText
-                    )
-                    await page.waitForSelector('[aria-label="Done"]')
-                    await page.click('[aria-label="Done"]')
-                    connectionReqSent = connectionReqSent + 1
-                    console.log(`<========== Connection request sent to ${connectionReqSent} people ==========>`)
+
+
+                    if (await page.$('input[id="email"]') !== null){
+                            await page.waitForSelector('[aria-label="Cancel adding a note"]')
+                            await page.click('[aria-label="Dismiss"]')
+                    }else{
+                        await page.type(
+                            '#custom-message',
+                            marketText
+                        )
+                        await page.waitForSelector('[aria-label="Done"]')
+                        await page.click('[aria-label="Done"]')
+                        connectionReqSent = connectionReqSent + 1
+                        console.log(`<========== Connection request sent to ${connectionReqSent} people ==========>`)
+                    }
+
                 } else {
                     console.log('<========== Connection request to this person has been already sent ==========>')
                 }
@@ -100,7 +109,7 @@ try {
                 await browser.close();
             }
         }catch(error){
-            console.log('<========== App broke please restart ==========>')
+            console.log(error, '<========== App broke please restart ==========>')
         }
     }
 
