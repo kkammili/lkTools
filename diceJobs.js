1ceff8160c706091b02fe191889666d16ee2de02
krishna <krishna@kkdevice.lan>
2022-07-17 17:50:46 -0500

    fix(diceJobs): some more fixes for dice jobs

--- diceJobs.js
+++ diceJobs.js
@@ -18,7 +18,7 @@ const puppeteer = require('puppeteer');
     let currPage = 1
     let jobApplied = 0
     const reapply = async () =>{
-        await page.goto(`https://www.dice.com/jobs?q=react.js&countryCode=US&radius=30&radiusUnit=mi&page=${currPage}&pageSize=20&filters.employmentType=CONTRACTS&filters.isRemote=true&language=en&eid=S2Q_,gKQ_`)
+        await page.goto(`https://www.dice.com/jobs?q=node js&countryCode=US&radius=30&radiusUnit=mi&page=${currPage}&pageSize=20&filters.employmentType=CONTRACTS&filters.isRemote=true&language=en&eid=S2Q_,gKQ_`)
         await page.waitFor(5000)
 
         // jobs on current page
@@ -31,14 +31,15 @@ const puppeteer = require('puppeteer');
 
         //visiting job link page in loop
         for(let j=0; j<jobsOnCurrPage.length; j++){
+            let page2 = await browser.newPage()
             try{
-                let page2 = await browser.newPage()
                 await page2.setViewport({width: 1800, height: 1080});
                 await page2.goto(jobsOnCurrPage[j])
 
 
                 //pulling apply button
                 const applyNowButton = (await page2.$x("/html/body/div[3]/div[5]/div[2]/div[2]/div/div[2]/div[1]/dhi-wc-apply-button"))[0]
+
                 await applyNowButton.click()
 
                 const nextButton = await page2.waitFor('#app > div > span > div > div.step-one > div.navigation-buttons > button.btn.btn-primary.btn-next.btn-block')
@@ -47,11 +48,13 @@ const puppeteer = require('puppeteer');
                 const applyButton = await page2.waitFor('#app > div > span > div > div.step-four > div.navigation-buttons > button.btn.btn-primary.btn-next.btn-split')
                 await applyButton.click()
 
-                console.log(`currently applying ${j} of ${jobsOnCurrPage.length} on page ${currPage}`)
+                console.log(`currently applying ${j} of ${jobsOnCurrPage.length} on page ${currPage}, job applied in total ${jobApplied}`)
                 await page2.close()
+                jobApplied = jobApplied + 1
 
             }catch(e){
                 console.log('errored out', e)
+                await page2.close()
             }
         }
 
